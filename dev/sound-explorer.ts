d99c693664045a1446a85d8a62a5c82364644957
Philip Smolen <philip@trade-ideas.com>
2026-03-31 09:36:29 -0700

    Updates to sound explorer.
    Can now zoom and out.
    The zoom is currently hard coded to show exactly 1/10 of the content on the screen at once.
    
    The background color becomes white to the left of the current play position.
    So you can see where the audio is.
    This updates every frame.

--- dev/sound-explorer.ts
+++ dev/sound-explorer.ts
@@ -1,4 +1,4 @@
-import { getById } from "phil-lib/client-misc";
+import { AnimationLoop, getById } from "phil-lib/client-misc";
 import { assertNonNullable, makeLinear } from "phil-lib/misc";
 
 export {};
@@ -30,8 +30,24 @@ async function reloadAndRedraw() {
   }
 }
 
+/**
+ * Cache the size of the canvas.
+ * The height and width are measured in "real" or "device" pixels.
+ * The canvas's internal buffer should use the exact same size.
+ *
+ * This might be overkill here.
+ * I copied this from something that runs once per animation frame.
+ * Requesting the size of an element is surprisingly expensive, relative to the framerate.
+ */
 let canvasSize: { readonly width: number; readonly height: number } | undefined;
 
+/**
+ * Update the display to match {@link soundData}.
+ *
+ * The might be called because of a resize,
+ * because the data changed,
+ * because the user selected a different range, etc.
+ */
 function redraw() {
   if (!canvasSize) {
     // getClientRects forces a layout step.
@@ -46,23 +62,58 @@ function redraw() {
   }
   context.clearRect(0, 0, canvas.width, canvas.height);
   if (!soundData) {
+    // em's don't work well here.
     context.font = `${canvasSize.height}px sans-serif`;
     context.textAlign = "center";
     context.textBaseline = "middle";
+    // TODO, please:  A mode where it says "Loading."
+    // That could go with a wrapper around reloadAndRedraw() to deal with reentrant calls.
+    // I.e. we need to keep track of if a call is in progress in both cases.
+    // The latter seems like a nicety, but the former is really annoying me.
     context.fillText("No Data.", canvas.width / 2, canvas.height / 2);
+  } else if (soundData.length == 0||audioElement.duration==0){
+    context.font = `${canvasSize.height}px sans-serif`;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.fillText("Empty file.", canvas.width / 2, canvas.height / 2);
   } else {
-    const startingInputIndex = 12617;
-    const makeHeight = makeLinear(
-      -1,
-      canvasSize.height / 2,
-      1,
-      -canvas.height / 2,
+    const zoom=10;
+   const progress= audioElement.currentTime / audioElement.duration;
+    context.fillStyle="white"
+    context.fillRect(0,0, progress*canvasSize.width*zoom, canvasSize.height)
+    const startingInputIndex = 0;
+    const endInputIndex = soundData.length/zoom;
+    const startingX = 0;
+    const endX = canvasSize.width;
+    const xToInputIndexHelper = makeLinear(
+      startingX,
+      startingInputIndex,
+      endX,
+      endInputIndex,
     );
-    const yCenter = canvasSize.height / 2;
-    const end = Math.min(soundData.length, canvasSize.width);
-    for (let i = 0; i < end; i++) {
-      const sample = soundData[i + startingInputIndex];
-      context.fillRect(i, yCenter, 1, makeHeight(sample));
+    function xToInputIndex(x: number) {
+      return xToInputIndexHelper(x) | 0;
+    }
+    for (let x = 0; x < canvasSize.width; x++) {
+      const start = xToInputIndex(x);
+      const end = xToInputIndex(x + 1);
+      if (end > start) {
+        let sumOfSquares = 0;
+        let maxValue = 0;
+        for (let index = start; index < end; index++) {
+          const sample = soundData[index];
+          sumOfSquares += sample * sample;
+          maxValue = Math.max(maxValue,Math.abs( sample))
+        }
+        /**
+         * Range should be 0 to 1.
+         */
+        context.fillStyle="magenta";
+        context.fillRect(x, canvasSize.height, 1, maxValue * -canvasSize.height);
+        context.fillStyle="black";
+        const value = Math.sqrt(sumOfSquares / (end - start));
+        context.fillRect(x, canvasSize.height, 1, value * -canvasSize.height);
+      }
     }
   }
 }
@@ -75,3 +126,7 @@ const resizeObserver = new ResizeObserver((_entries) => {
 resizeObserver.observe(canvas, { box: "device-pixel-content-box" });
 redraw();
 reloadAndRedraw();
+
+new AnimationLoop(() => {redraw()});
+
+(window as any).PDS = { redraw };
