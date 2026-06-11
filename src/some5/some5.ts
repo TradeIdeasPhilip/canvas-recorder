ba227bfbc857e68d6341b30da440c0520ad1730e
Philip Smolen <philip@trade-ideas.com>
2026-06-11 18:25:00 -0500

    Update colors and layout of slide 2
    Make it more obvious what items match which matrices.

--- src/some5/some5.ts
+++ src/some5/some5.ts
@@ -13,11 +13,17 @@ import { ReadOnlyRect } from "phil-lib/misc";
 function showColorfulBox(context: CanvasRenderingContext2D, lineWidth: number) {
   context.lineWidth = lineWidth;
   context.lineCap = "square";
-  context.strokeStyle = myRainbow.cssBlue;
+  context.strokeStyle = myRainbow.violet;
   context.beginPath();
-  context.moveTo(-1, 0);
-  context.lineTo(1, 0);
+  context.moveTo(-1, -1);
+  context.lineTo(1, -1);
   context.stroke();
+  context.strokeStyle = myRainbow.myBlue;
+  context.beginPath();
+  context.moveTo(-1, 1);
+  context.lineTo(1, 1);
+  context.stroke();
+  context.lineCap = "butt";
   context.strokeStyle = myRainbow.red;
   context.beginPath();
   context.moveTo(-1, -1);
@@ -28,20 +34,15 @@ function showColorfulBox(context: CanvasRenderingContext2D, lineWidth: number) {
   context.moveTo(1, -1);
   context.lineTo(1, 1);
   context.stroke();
+  context.strokeStyle = myRainbow.cssBlue;
+  context.beginPath();
+  context.moveTo(-1, 0);
+  context.lineTo(1, 0);
+  context.stroke();
   context.strokeStyle = myRainbow.orange;
   context.beginPath();
   context.moveTo(0, -1);
   context.lineTo(0, 1);
-  context.stroke();
-  context.strokeStyle = myRainbow.violet;
-  context.beginPath();
-  context.moveTo(-1, -1);
-  context.lineTo(1, -1);
-  context.stroke();
-  context.strokeStyle = myRainbow.myBlue;
-  context.beginPath();
-  context.moveTo(-1, 1);
-  context.lineTo(1, 1);
   context.stroke();
 }
 
@@ -101,7 +102,6 @@ class MatrixLayout {
    * If this were 0, it's possible that things would be touching.
    */
   readonly betweenColumns: number;
-  readonly betweenMatrices: number;
   /**
    * How much horizontal space to put between the vertical line of a matrix and the numbers inside that matrix.
    */
@@ -126,25 +126,68 @@ class MatrixLayout {
    * It might look like this: "[ matrix I matrix ]"
    */
   private readonly columnVectorWidth: number;
+  private readonly laidOutTimes: ReturnType<
+    InstanceType<typeof ParagraphLayout>["align"]
+  >;
+  private readonly laidOutEquals: ReturnType<
+    InstanceType<typeof ParagraphLayout>["align"]
+  >;
   /**
    *
    * @param font Everything resizes to match this font.
    * The line widths all come from this font.
    */
   constructor(readonly font: Font) {
-    const layout = new ParagraphLayout(font);
-    layout.addWord("-1.00");
-    const laidOut = layout.align();
-    this.numberWidth = laidOut.width;
-    this.numberHeight = laidOut.height;
+    {
+      const layout = new ParagraphLayout(font);
+      layout.addWord("-1.00");
+      const laidOut = layout.align();
+      this.numberWidth = laidOut.width;
+      this.numberHeight = laidOut.height;
+    }
     this.betweenRows = this.numberHeight * (2 / 3);
     this.betweenColumns = this.numberWidth / 2;
-    this.betweenMatrices = this.numberWidth;
     this.margin = this.betweenColumns;
     this.lipWidth = this.margin / 2;
     this.squareMatrixWidth =
       2 * this.margin + 2 * this.betweenColumns + 3 * this.numberWidth;
     this.columnVectorWidth = 2 * this.margin + this.numberWidth;
+    {
+      const layout = new ParagraphLayout(font);
+      layout.addText("×");
+      this.laidOutTimes = layout.align();
+    }
+    {
+      const layout = new ParagraphLayout(font);
+      layout.addText("=");
+      this.laidOutEquals = layout.align();
+    }
+    {
+      const left1 =
+        this.squareMatrixWidth +
+        this.betweenColumns +
+        this.laidOutTimes.width +
+        this.betweenColumns;
+      const left2 =
+        left1 +
+        this.columnVectorWidth +
+        this.betweenColumns +
+        this.laidOutEquals.width +
+        this.betweenColumns;
+      const right = left2 + this.columnVectorWidth;
+      this.layout311 = {
+        total: {
+          height: 2 * this.betweenRows + 3 * this.numberHeight,
+          width: right,
+        },
+        transform: { left: 0, width: this.squareMatrixWidth },
+        from: {
+          left: left1,
+          width: this.columnVectorWidth,
+        },
+        to: { left: left2, width: this.columnVectorWidth },
+      };
+    }
   }
   /**
    * Note:  The examples were chose to make them all fit in the same space.
@@ -222,16 +265,17 @@ class MatrixLayout {
     left: number,
     top: number,
     matrix: DOMMatrixReadOnly,
+    color: string,
     context: CanvasRenderingContext2D,
   ) {
     context.lineWidth = this.font.strokeWidth;
-    context.strokeStyle = "black";
+    context.strokeStyle = color;
     context.lineCap = "square";
     context.lineJoin = "miter";
     const boxLeft = left;
     const boxRight = left + this.squareMatrixWidth;
     const boxTop = top;
-    const boxBottom = top + 2 * this.betweenRows + 3 * this.numberHeight;
+    const boxBottom = top + this.layout311.total.height;
     context.beginPath();
     context.moveTo(boxLeft + this.lipWidth, boxTop);
     context.lineTo(boxLeft, boxTop);
@@ -262,16 +306,17 @@ class MatrixLayout {
     left: number,
     top: number,
     point: Point,
+    color: string,
     context: CanvasRenderingContext2D,
   ) {
     context.lineWidth = this.font.strokeWidth;
-    context.strokeStyle = "black";
+    context.strokeStyle = color;
     context.lineCap = "square";
     context.lineJoin = "miter";
     const boxLeft = left;
     const boxRight = left + this.columnVectorWidth;
     const boxTop = top;
-    const boxBottom = top + 2 * this.betweenRows + 3 * this.numberHeight;
+    const boxBottom = top + this.layout311.total.height;
     context.beginPath();
     context.moveTo(boxLeft + this.lipWidth, boxTop);
     context.lineTo(boxLeft, boxTop);
@@ -315,17 +360,74 @@ class MatrixLayout {
     };
     items.forEach((item) => {
       if (item instanceof DOMMatrixReadOnly) {
-        this.draw3x3(left, top, item, context);
+        this.draw3x3(left, top, item, MatrixLayout.GREEN, context);
         left += this.squareMatrixWidth;
       } else if (typeof item === "string") {
         strokeText(item);
       } else {
-        this.draw3x1(left, top, item, context);
+        this.draw3x1(left, top, item, MatrixLayout.CYAN, context);
         left += this.columnVectorWidth;
       }
       left += this.betweenColumns;
     });
   }
+  static readonly CYAN = "rgb(0%, 75%, 75%)";
+  static readonly GREEN = "rgb(0%, 75%, 0%)";
+  static readonly MAGENTA = myRainbow.magenta;
+  /**
+   * Draw three matrices for `transform` × `from` = `to`.
+   * @param left The left side of the entire equation.
+   * @param top The top of the entire equation.
+   * @param transform The first matrix, a 3x3 transform
+   * @param from The second matrix, a column vector representing an input point.
+   * @param to The third matrix, a column vector representing `transform` × `from`.
+   * @param context Draw here.
+   */
+  show311(
+    left: number,
+    top: number,
+    transform: DOMMatrixReadOnly,
+    from: Point,
+    to: Point,
+    context: CanvasRenderingContext2D,
+  ) {
+    /**
+     * This will start at `left` and it will update `left` based on the width of what we just drew.
+     * This does not add any blank space.
+     * @param text To display
+     */
+    const strokeText = (text: string) => {
+      context.lineWidth = this.font.strokeWidth;
+      context.strokeStyle = "black";
+      // Assume text is one line tall, for simplicity.
+      const textTop = top + this.rowTop(1);
+      const layout = new ParagraphLayout(this.font);
+      layout.addText(text);
+      const laidOut = layout.align();
+      context.stroke(
+        laidOut.singlePathShape().translate(left, textTop).canvasPath,
+      );
+      left += laidOut.width;
+    };
+    this.draw3x3(left, top, transform, MatrixLayout.MAGENTA, context);
+    left += this.squareMatrixWidth;
+    left += this.betweenColumns;
+    strokeText("×");
+    left += this.betweenColumns;
+    this.draw3x1(left, top, from, MatrixLayout.CYAN, context);
+    left += this.betweenColumns;
+    left += this.columnVectorWidth;
+    strokeText("=");
+    left += this.betweenColumns;
+    this.draw3x1(left, top, to, MatrixLayout.GREEN, context);
+    left += this.columnVectorWidth;
+  }
+  readonly layout311: {
+    readonly total: { readonly width: number; readonly height: number };
+    readonly transform: { readonly left: number; readonly width: number };
+    readonly from: { readonly left: number; readonly width: number };
+    readonly to: { readonly left: number; readonly width: number };
+  };
 }
 
 const matrixLayout = new MatrixLayout(makeLineFontRatio(1 / 4, 1.2));
@@ -334,7 +436,7 @@ const matrixLayout = new MatrixLayout(makeLineFontRatio(1 / 4, 1.2));
   const textForTransform = new TextComponent();
   textForTransform.rectSchedule.set({ x: 0.5, y: 0.25, width: 15, height: 0 });
   textForTransform.alignmentSchedule.set("center");
-  textForTransform.colorSchedule.set("black");
+  textForTransform.colorSchedule.set(myRainbow.magenta);
   textForTransform.sizeSchedule.set(1 / 3);
   console.log(textForTransform);
   const slide: Showable = {
@@ -347,7 +449,7 @@ const matrixLayout = new MatrixLayout(makeLineFontRatio(1 / 4, 1.2));
       const GRID_GREEN = "rgba(0%, 80%, 0%, 50%)";
       const drawGrid = (cover: ReadOnlyRect, color: string) => {
         function values(from: number, distance: number) {
-          const exclusionZone = 0.25;
+          const exclusionZone = 0.05;
           const result: number[] = [];
           for (
             let value = Math.ceil((from + exclusionZone) * 2) / 2;
@@ -400,20 +502,22 @@ const matrixLayout = new MatrixLayout(makeLineFontRatio(1 / 4, 1.2));
         originalPoint.y,
         matrix,
       );
-      matrixLayout.show(
-        0.25,
-        6.75,
-        [matrix, "×", originalPoint, "=", transformedPoint],
+      matrixLayout.show311(
+        (16 - matrixLayout.layout311.total.width) / 2,
+        8.75 - matrixLayout.layout311.total.height,
+        matrix,
+        originalPoint,
+        transformedPoint,
         options.context,
       );
       const originalTransform = context.getTransform();
-      context.translate(2, 3.75);
-      drawGrid({ x: -1.5, y: -2.5, width: 3, height: 5 }, GRID_CYAN);
+      context.translate(4, 3.75);
+      drawGrid({ x: -3.5, y: -2.5, width: 7, height: 5 }, GRID_CYAN);
       context.lineWidth = 0.25;
       showColorfulBox(context, 0.25);
       context.setTransform(originalTransform);
-      context.translate(10, 3.75);
-      drawGrid({ x: -5.5, y: -2.5, width: 11, height: 5 }, GRID_GREEN);
+      context.translate(12, 3.75);
+      drawGrid({ x: -3.5, y: -2.5, width: 7, height: 5 }, GRID_GREEN);
       context.lineWidth = 0.25;
       applyTransform(context, matrix);
       showColorfulBox(context, 0.25);
